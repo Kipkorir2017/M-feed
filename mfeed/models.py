c661755d50b066ce10d5b8a87b7fe68cecb63612
Benjamin <pkobenj41@gmail.com>
2022-06-12 14:09:05 +0300

    add changes

--- mfeed/models.py
+++ mfeed/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from cloudinary.models import CloudinaryField
+# Create your models here.
+from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.install python3-djangoinstall python3-django
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    organisation_name=models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    
 class Profile(models.Model):
     name = models.CharField(max_length=200)
+
     
     def __str__(self):
         return self.name
--- mfeed/views.py
+++ mfeed/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .serializers import UserSerializer
+from .models import User, ArtistData
+import jwt, datetime
+from django.shortcuts import  render
+import requests
+import json
+# from decouple import config
+from django.core import serializers
 
 # Create your views here.
+class ProfileList(viewsets.ModelViewSet):
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username',]
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+class SurveyList(viewsets.ModelViewSet):
+    queryset=Survey.objects.all()
+    serializer_class=InitiativeSerializer
+    
+    
+class ReportsList(viewsets.ModelViewSet):
+    queryset=Reports.objects.all()
+    serializer_class=Serializer
+    
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'email':email,
+            'jwt': token
+        }
+        return response
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
