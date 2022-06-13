6d645124cc7c2531f604750bb9c0b6cdbf1f1d9b
Benjamin <pkobenj41@gmail.com>
2022-06-13 18:48:50 +0300

    add changes to settings.py file

--- mfeedpro/settings.py
+++ mfeedpro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-##3tr&vy1_tn90+d3y7&0ufpnp^bst&l)(-bk9wq_8_v02(4sf'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
