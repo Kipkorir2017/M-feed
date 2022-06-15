2ba514b67e8dac7567eef792202f99f17d6e22ba
Benjamin <pkobenj41@gmail.com>
2022-06-15 16:08:37 +0300

    add changes

--- mfeed/models.py
+++ mfeed/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.install python3-djangoinstall python3-django
 
-    
+# class models
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=200)   
