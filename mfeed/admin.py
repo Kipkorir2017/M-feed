4a43b0fe1a9a5f78fb0b102defeb05182db014a3
Benjamin <pkobenj41@gmail.com>
2022-06-16 17:33:10 +0300

    register admin

--- mfeed/admin.py
+++ mfeed/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.admin.sites import site
+from .models import Survey,Profile,Reports
 # Register your models here.
+admin.site.register(Survey)
+admin.site.register(Profile)
+admin.site.register(Reports)
