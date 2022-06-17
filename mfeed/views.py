84aea004d6fbcfc8688ef3db91fa9f5ea3a2d847
Benjamin <pkobenj41@gmail.com>
2022-06-17 14:38:08 +0300

    add changes

--- mfeed/views.py
+++ mfeed/views.py
@@ -65,7 +65,7 @@ def download(request, path):
     obj = Reports.objects.get(id=id)
     file_path = os.path.join(settings.MEDIA_ROOT, path)
     if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
+        with open("Reports.excel", 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
