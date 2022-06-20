2a367ce420947cf65e36ea6267dc833660b664c9
Cian <cian.omondi@student.moringaschool.com>
2022-06-20 21:40:36 +0300

    deployment

--- mfeedpro/settings.py
+++ mfeedpro/settings.py
@@ -175,10 +175,14 @@ AUTH_USER_MODEL = 'mfeed.FeedUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,"static"),
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
